84f1f7e0ecc056bc04da0b36b59122feb4215d99
Elliott Blocha <eblocha@gmail.com>
2022-01-19 20:24:27 -0500

    refactor useSetDisplay to take the profile as an argument, and return async results

--- src/components/Layout/TopInfo.tsx
+++ src/components/Layout/TopInfo.tsx
@@ -1,9 +1,7 @@
 import useActiveLift from '../../hooks/useActiveLift';
 import useActiveSet from '../../hooks/useActiveSet';
 import useDay from '../../hooks/useDay';
-import useMaxes from '../../hooks/useMaxes';
 import useProfile from '../../hooks/useProfile';
-import useProgram from '../../hooks/useProgram';
 import useSetDisplay from '../../hooks/useSetDisplay';
 
 const TopInfo = () => {
@@ -12,30 +10,27 @@ const TopInfo = () => {
   const [day] = useDay();
   const [profile] = useProfile();
 
-  const { data: program } = useProgram(profile);
-  const { data: maxes } = useMaxes(profile);
-
   const current = useSetDisplay({
-    program: program || {},
+    profile,
     day,
     lift: lift,
-    maxes: maxes ? maxes[maxes.length - 1] || {} : {},
     set: set,
   });
 
   const next = useSetDisplay({
-    program: program || {},
+    profile,
     day,
     lift: lift,
-    maxes: maxes ? maxes[maxes.length - 1] || {} : {},
     set: set + 1,
   });
 
   return (
     <div className="flex flex-row items-center overflow-hidden w-full h-full p-5 justify-between">
       <div className="flex flex-col grow overflow-hidden">
-        <div className="overflow-hidden text-7xl mb-4">{current}</div>
-        <div className="overflow-hidden text-5xl opacity-50">Next: {next}</div>
+        <div className="overflow-hidden text-7xl mb-4">{current.data}</div>
+        <div className="overflow-hidden text-5xl opacity-50">
+          Next: {next.data}
+        </div>
       </div>
       <div className="shrink-0 h-32 w-32">
         <div className="h-full rounded-full w-full bg-blue-500"></div>
--- src/hooks/useSetDisplay.ts
+++ src/hooks/useSetDisplay.ts
@@ -1,18 +1,12 @@
-import {
-  CustomLift,
-  CustomLiftSet,
-  LiftTypes,
-  MainLift,
-  Maxes,
-  Program,
-} from '../api';
+import { CustomLift, CustomLiftSet, LiftTypes, MainLift } from '../api';
 import { currentLift, displaySet } from '../utils/program';
 import { roundWeight } from '../utils/weight';
+import useMaxes from './useMaxes';
+import useProgram from './useProgram';
 import useUnits from './useUnits';
 
 type Args = {
-  program: Program;
-  maxes: Maxes;
+  profile: string;
   day: number;
   lift: number;
   set: number;
@@ -27,39 +21,124 @@ const fullName = (
   return (includeName ? `${lift.name} ` : '') + displaySet(set);
 };
 
+type WrapArgs = {
+  progIsError: boolean;
+  progIsLoading: boolean;
+  progError: unknown;
+  maxesIsError: boolean;
+  maxesIsLoading: boolean;
+  maxesError: unknown;
+};
+
+const wrapReturn = ({
+  maxesError,
+  maxesIsError,
+  maxesIsLoading,
+  progError,
+  progIsError,
+  progIsLoading,
+}: WrapArgs) => {
+  const error = maxesError || progError;
+  const isError = maxesIsError || progIsError;
+  const isLoading = maxesIsLoading || progIsLoading;
+
+  return {
+    error,
+    isError,
+    isLoading,
+  };
+};
+
 const useSetDisplay = ({
-  program,
-  maxes,
+  profile,
   day,
   lift,
   set,
   includeName = true,
-}: Args) => {
-  const currLift = currentLift(program, day, lift);
+}: Args): {
+  data: string | null;
+  error: unknown;
+  isError: boolean;
+  isLoading: boolean;
+} => {
+  const {
+    data: program,
+    isError: progIsError,
+    isLoading: progIsLoading,
+    error: progError,
+  } = useProgram(profile);
+
+  const {
+    data: maxesList,
+    isError: maxesIsError,
+    isLoading: maxesIsLoading,
+    error: maxesError,
+  } = useMaxes(profile);
+
   const [units] = useUnits();
 
-  if (!currLift) return null;
+  const metaData = {
+    maxesError,
+    maxesIsError,
+    maxesIsLoading,
+    progError,
+    progIsError,
+    progIsLoading,
+  };
+
+  if (!program || !maxesList)
+    return {
+      data: null,
+      ...wrapReturn(metaData),
+    };
+
+  const maxes = maxesList[maxesList.length - 1] || {};
+
+  const currLift = currentLift(program, day, lift);
+
+  if (!currLift)
+    return {
+      data: null,
+      ...wrapReturn(metaData),
+    };
 
   switch (currLift.type) {
     case LiftTypes.CUSTOM: {
       const sets = currLift.sets || [];
       const currSet = sets[set];
-      if (!currSet) return null;
-      return fullName(currLift, currSet, includeName);
+      if (!currSet)
+        return {
+          data: null,
+          ...wrapReturn(metaData),
+        };
+      return {
+        data: fullName(currLift, currSet, includeName),
+        ...wrapReturn(metaData),
+      };
     }
     case LiftTypes.MAIN: {
       const sets = currLift.sets || [];
       const currSet = sets[set];
-      if (!currSet) return null;
+      if (!currSet)
+        return {
+          data: null,
+          ...wrapReturn(metaData),
+        };
+
+      const max = maxes[currLift.base];
+
       const setForDisplay: CustomLiftSet = {
         id: currSet.id,
         reps: currSet.reps,
-        weight: roundWeight(
-          currSet.percentage * (maxes[currLift.base] || 0),
-          units
-        ),
+        weight:
+          max !== undefined
+            ? roundWeight(currSet.percentage * max, units)
+            : `${currSet.percentage * 100}% of max`,
       };
-      return fullName(currLift, setForDisplay, includeName);
+      return {
+        data: fullName(currLift, setForDisplay, includeName),
+        ...wrapReturn(metaData),
+      };
     }
   }
 };
