e93e7346a5517934cab3220d69372a822cc4a0e7
eblocha <eblocha@protonmail.com>
2022-05-01 10:10:51 -0400

    display message in top info if empty

--- src/components/Layout/TopInfo.tsx
+++ src/components/Layout/TopInfo.tsx
@@ -26,13 +26,9 @@ const TopInfo = () => {
 
   return (
     <div className="flex flex-row items-center overflow-hidden w-full h-full p-5 justify-between">
-      <div className="flex flex-col grow overflow-hidden">
-        <div className="overflow-hidden text-7xl mb-4">
-          {current.data || ''}
-        </div>
-        <div className="overflow-hidden text-5xl opacity-50">
-          Next: {next.data}
-        </div>
+      <div className="flex flex-col grow">
+        <div className="text-7xl mb-4">{current.data || 'Nothing!'}</div>
+        <div className="text-5xl opacity-50">Next: {next.data || 'Done!'}</div>
       </div>
       <div className="shrink-0 h-32 w-32">
         <div className="h-full rounded-full w-full bg-blue-500"></div>
