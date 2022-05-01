c2921f7bda5ce8f810929e6d4e8627fa8b06cd4f
eblocha <eblocha@protonmail.com>
2022-05-01 17:01:09 -0400

    move mic button outside TopInfo component

--- src/components/Layout/Dashboard.tsx
+++ src/components/Layout/Dashboard.tsx
@@ -5,8 +5,11 @@ import TopInfo from './TopInfo';
 const Dashboard = () => {
   return (
     <div className="flex flex-col overflow-hidden h-full w-full">
-      <div className="shrink-0 overflow-hidden">
+      <div className="shrink-0 overflow-hidden flex flex-row p-5 items-start">
         <TopInfo />
+        <div className="shrink-0 h-32 w-32">
+          <div className="h-full rounded-full w-full bg-blue-500"></div>
+        </div>
       </div>
       <div className="grow overflow-hidden">
         <MainView />
--- src/components/Layout/TopInfo.tsx
+++ src/components/Layout/TopInfo.tsx
@@ -25,14 +25,9 @@ const TopInfo = () => {
   });
 
   return (
-    <div className="flex flex-row items-center overflow-hidden w-full h-full p-5 justify-between">
-      <div className="flex flex-col grow">
-        <div className="text-7xl mb-4">{current.data || 'Nothing!'}</div>
-        <div className="text-5xl opacity-50">Next: {next.data || 'Done!'}</div>
-      </div>
-      <div className="shrink-0 h-32 w-32">
-        <div className="h-full rounded-full w-full bg-blue-500"></div>
-      </div>
+    <div className="flex flex-col grow">
+      <div className="text-7xl mb-4">{current.data || 'Nothing!'}</div>
+      <div className="text-5xl opacity-50">Next: {next.data || 'Done!'}</div>
     </div>
   );
 };
