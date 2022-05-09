3be02683bb1b50be91be94a409187bf6e936cf33
eblocha <eblocha@protonmail.com>
2022-05-08 20:46:43 -0400

    remove z index from animation variants

--- src/components/Layout/DayColumn.tsx
+++ src/components/Layout/DayColumn.tsx
@@ -8,18 +8,15 @@ import CurrentDay from './CurrentDay';
 const variants: Variants = {
   enter: (direction: number) => {
     return {
-      zIndex: 0,
       x: direction > 0 ? 1000 : -1000,
     };
   },
   center: {
-    zIndex: 1,
     x: 0,
     opacity: 1,
   },
   exit: (direction: number) => {
     return {
-      zIndex: 0,
       x: direction < 0 ? 1000 : -1000,
       opacity: 0,
     };
