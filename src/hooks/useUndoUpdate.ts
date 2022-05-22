aa94c0e7a6f7d1fe32f3474a1e03b1b6be2ff23c
eblocha <eblocha@protonmail.com>
2022-05-22 10:29:57 -0400

    display error message if undo update fails

--- src/hooks/useUndoUpdate.ts
+++ src/hooks/useUndoUpdate.ts
@@ -1,16 +1,38 @@
 import { useCallback } from 'react';
+import useStore, { Store } from '../stores';
 import { useDeleteMaxes } from './useMaxes';
 import useProfile from './useProfile';
 import { useDeleteReps } from './useReps';
 
+const selectAddMessage = (state: Store) => state.addMessage;
+
 export const useUndoUpdate = () => {
   const [profile] = useProfile();
+  const addMessage = useStore(selectAddMessage);
 
   const { mutate: deleteReps } = useDeleteReps(profile);
   const { mutate: deleteMaxes } = useDeleteMaxes(profile);
 
   return useCallback(() => {
-    deleteMaxes();
-    deleteReps();
-  }, [deleteMaxes, deleteReps]);
+    deleteMaxes(undefined, {
+      onError: (error) => {
+        addMessage({
+          level: 'error',
+          message: 'Failed to undo max update',
+          timeout: 5000,
+        });
+        console.error(error);
+      },
+    });
+    deleteReps(undefined, {
+      onError: (error) => {
+        addMessage({
+          level: 'error',
+          message: 'Failed to undo rep update',
+          timeout: 5000,
+        });
+        console.error(error);
+      },
+    });
+  }, [addMessage, deleteMaxes, deleteReps]);
 };
